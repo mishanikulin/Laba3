379d26c2fba80672dfb7f85675545517a7a577c1
mishanikulin <149046523+mishanikulin@users.noreply.github.com>
2024-12-11 09:50:51 +0300

    Update main.py

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ from fastapi.responses import HTMLResponse
 app = FastAPI()
 
 # Настройка базы данных MySQL
-SQLALCHEMY_DATABASE_URL = "mysql+pymysql://isp_p_Zobov:12345@77.91.86.135/isp_p_Zobov"
+SQLALCHEMY_DATABASE_URL = "mysql+pymysql://isp_p_Nikulin:12345@77.91.86.135/isp_p_Nikulin"
 SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
